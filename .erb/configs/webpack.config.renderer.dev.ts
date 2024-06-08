64813b92e2a47b28be3e8fe0baa2a9133474d804
Tim <root@root.com>
2024-06-08 12:33:14 +0200

    Installed tailwind

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -62,27 +62,6 @@ const configuration: webpack.Configuration = {
 
   module: {
     rules: [
-      {
-        test: /\.s?(c|a)ss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          'sass-loader',
-        ],
-        include: /\.module\.s?(c|a)ss$/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        exclude: /\.module\.s?(c|a)ss$/,
-      },
       // Fonts
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -111,6 +90,11 @@ const configuration: webpack.Configuration = {
           },
           'file-loader',
         ],
+      },
+      {
+        test: /\.css$/,
+        include: [webpackPaths.srcRendererPath],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,7 +8,8 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1 className="text-green-500">Scuuuuur</h1>
+      <p className="bg-gray-500 text-center">okii</p>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
