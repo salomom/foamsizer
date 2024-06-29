5f67f297ae6275a601504e947ae873260109f705
Tim <hrshrs740@gmail.com>
2024-06-29 13:58:53 +0200

    Crop Image Rectangle

--- src/renderer/components/scanImage.jsx
+++ src/renderer/components/scanImage.jsx
@@ -1,10 +1,11 @@
 import Button from './buttons';
 import useImage from 'use-image';
 import { useEffect, useState, useRef } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
+import { Stage, Layer, Image, Rect, Transformer } from 'react-konva';
 
 export default function ScanImage({ currentPath }) {
   const [image, setImage] = useState('https://placehold.co/500x500');
+  const [sizeRect, setSizeRect] = useState({ x: 0, y: 0, width: 0, height: 0 });
   const imgPath = currentPath + '/scan.png';
 
   async function scan() {
@@ -28,6 +29,10 @@ export default function ScanImage({ currentPath }) {
     setImage('data:image/jpg;base64,' + imgBase64);
   }
 
+  async function cropImage() {
+    console.log(sizeRect);
+  }
+
   const firstRender = useRef(true);
 
   useEffect(() => {
@@ -42,20 +47,80 @@ export default function ScanImage({ currentPath }) {
 
   return (
     <div className="mt-10 mx-10 w-full">
-      <Button title="Scan" onClick={scan} big />
-      <ImageCrop image={image} />
+      <div className="flex">
+        <Button title="Scan" onClick={scan} big />
+        <Button title="Crop" onClick={cropImage} big />
+      </div>
+      <ImageCrop image={image} setSizeRect={setSizeRect} />
     </div>
   );
 }
 
-function ImageCrop({ image }) {
+function ImageCrop({ image, setSizeRect }) {
   const [konvaImage] = useImage(image);
+  const sizeRect = useRef(null);
+  const sizeTransformer = useRef(null);
+
+  useEffect(() => {
+    if (sizeRect.current && sizeTransformer.current) {
+      sizeTransformer.current.nodes([sizeRect.current]);
+      sizeTransformer.current.getLayer().batchDraw();
+    }
+  });
+
   return (
     <div className="mt-4">
       {image && (
         <Stage width={508} height={699} scaleX={0.2} scaleY={0.2}>
           <Layer>
             <Image image={konvaImage} />
+          </Layer>
+          <Layer>
+            <Rect
+              x={0}
+              y={0}
+              ref={sizeRect}
+              width={508}
+              height={699}
+              stroke="black"
+              strokeWidth={0}
+              draggable
+              onDragEnd={(e) => {
+                if (e.target.x() < 0) e.target.x(0);
+                if (e.target.y() < 0) e.target.y(0);
+                setSizeRect({
+                  x: parseInt(e.target.x()),
+                  y: parseInt(e.target.y()),
+                  width: parseInt(e.target.width()),
+                  height: parseInt(e.target.height()),
+                });
+              }}
+              onTransformEnd={(e) => {
+                const node = sizeRect.current;
+                const scaleX = node.scaleX();
+                const scaleY = node.scaleY();
+
+                node.scaleX(1);
+                node.scaleY(1);
+                setSizeRect({
+                  x: parseInt(node.x()),
+                  y: parseInt(node.y()),
+                  width: parseInt(node.width() * scaleX),
+                  height: parseInt(node.height() * scaleY),
+                });
+                e.target.width(node.width() * scaleX);
+                e.target.height(node.height() * scaleY);
+              }}
+            />
+            <Transformer
+              ref={sizeTransformer}
+              boundBoxFunc={(oldBox, newBox) => {
+                if (newBox.width < 5 || newBox.height < 5) {
+                  return oldBox;
+                }
+                return newBox;
+              }}
+            />
           </Layer>
         </Stage>
       )}
