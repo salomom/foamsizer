9f9f2021539c87e9cf32cd90f1dc226fc0966822
Tim <hrshrs740@gmail.com>
2024-07-11 19:19:18 +0200

    Read properties and main image

--- src/renderer/components/editTools.jsx
+++ src/renderer/components/editTools.jsx
@@ -1,10 +1,12 @@
 import Button from './buttons';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Stage, Layer, Image } from 'react-konva';
 import useImage from 'use-image';
 
 export default function EditTools({ currentPath, setCurrentPath }) {
   const [coverImage, setCoverImage] = useState('');
+  const [mainImage, setMainImage] = useState('');
+  const [properties, setProperties] = useState('');
 
   async function openFile() {
     const filePath = await window.electronAPI.openFile();
@@ -31,6 +33,54 @@ export default function EditTools({ currentPath, setCurrentPath }) {
     readCoverImage(filePath);
   }
 
+  async function readPropertyFile(filePath) {
+    const fileContent = await window.electronAPI.readFile(
+      filePath + '/properties.txt',
+    );
+    if (!fileContent) {
+      setProperties('');
+    } else {
+      const propertiesObj = {};
+      const lines = fileContent.split('\n');
+      lines.forEach((line) => {
+        const [key, value] = line.split(':');
+        if (!value) {
+          return;
+        }
+        if (value[0] === ' ') {
+          propertiesObj[key] = value.slice(1);
+        } else {
+          propertiesObj[key] = value;
+        }
+      });
+      setProperties(propertiesObj);
+    }
+  }
+
+  async function readMainImage(filePath) {
+    const imgBase64 = await window.electronAPI.openImage(
+      filePath + '/main.png',
+    );
+    if (!imgBase64) {
+      setMainImage('');
+      return;
+    }
+    setMainImage('data:image/jpg;base64,' + imgBase64);
+  }
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      if (currentPath) {
+        readMainImage(currentPath);
+        readPropertyFile(currentPath);
+      }
+      return;
+    }
+  });
+
   return (
     <div>
       <div className="text-white font-bold ml-10 my-4">
@@ -42,18 +92,22 @@ export default function EditTools({ currentPath, setCurrentPath }) {
         </div>
       </div>
       <div className="mx-10">
-        {coverImage && <OverlayCoverImage coverImage={coverImage} />}
+        {coverImage && (
+          <OverlayCoverImage mainImage={mainImage} coverImage={coverImage} />
+        )}
       </div>
     </div>
   );
 }
 
-function OverlayCoverImage({ coverImage }) {
-  const [konvaImage] = useImage(coverImage);
+function OverlayCoverImage({ mainImage, coverImage }) {
+  const [konvaMainImage] = useImage(mainImage);
+  const [konvaCoverImage] = useImage(coverImage);
   return (
     <Stage width={500} height={500}>
       <Layer>
-        <Image image={konvaImage} height={500} width={500} />
+        <Image image={konvaMainImage} height={500} width={500} />
+        <Image image={konvaCoverImage} height={500} width={500} opacity={0.5} />
       </Layer>
     </Stage>
   );
